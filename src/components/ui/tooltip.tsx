c4642a2a0663d855421c077449bdcc694bf95b82
ojus <guptaojas75@gmail.com>
2025-08-28 16:36:33 +0530

    dswrnk

---
+++ src/components/ui/tooltip.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+import * as React from "react"
+
+// Simple tooltip placeholders - focusing on SSR
+export const TooltipProvider = ({ children }: { children: React.ReactNode }) => <>{children}</>;
+export const Tooltip = ({ children }: { children: React.ReactNode }) => <>{children}</>;
+export const TooltipTrigger = ({ children }: { children: React.ReactNode }) => <>{children}</>;
+export const TooltipContent = ({ children }: { children: React.ReactNode }) => <>{children}</>;
